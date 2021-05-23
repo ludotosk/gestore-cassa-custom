2576e3e85c3ce2960f6536ade36dcabd236109f9
ludotosk <toscanoludovico@gmail.com>
2021-05-23 11:41:35 +0200

    client funzionante

---
+++ client/src/store/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import CryttoService from "../CryttoService";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    pubkey: '',
+    chiave: '',
+    token: '',
+    scontrino: []
+  },
+  mutations: {
+    setPubkey(state,payload) {
+      state.pubkey = payload;
+    },
+    setChiave(state,payload) {
+      state.chiave = payload;
+    },
+    setToken(state,payload) {
+      localStorage.token = payload;
+      state.token = payload;
+    },
+    aggiungiArticolo(state,payload){
+      state.scontrino.push(payload);
+    },
+    cancellaScontrino(state,){
+      state.scontrino = [];
+    },
+    cancellaArticolo(state,payload){
+      state.scontrino.splice(payload, 1);
+    }
+  },
+  actions: {
+    async setPubkey(state) {
+      const pubkey = await CryttoService.getPubkey();
+      state.commit('setPubkey', pubkey);
+    },
+    async setChiave(state) {
+      const chiave = await CryttoService.makeid(32);
+      state.commit('setChiave', chiave)  
+    }
+  },
+  modules: {
+  },
+  getters: {
+    getPubkey: state => state.pubkey,
+    getChiave: state => state.chiave,
+    getToken: state => state.token,
+    getScontrino: state => state.scontrino
+  }
+})
