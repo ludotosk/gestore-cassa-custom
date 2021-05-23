95de1a3857a3fcbd875da6943c7aee4e9dc53ac6
ludotosk <toscanoludovico@gmail.com>
2021-05-23 15:14:08 +0200

    diviso tutto in componenti

---
+++ client/src/components/ComponenteArticoli.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <!-- colonna lista prodotti -->
+    <div
+      class="flex flex-row bg-white rounded p-1 shadow lg:relative lg:top-0 bottom-32 fixed w-full"
+    >
+      <input
+        type="text"
+        class="m-1 block rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 w-full hover:border-blue-300"
+        v-model="filtro"
+      />
+      <button
+        v-if="prodotti != null"
+        class="bg-blue-300 p-2 rounded block m-1 hover:bg-blue-400"
+        @click.prevent="getProdottiFiltrati"
+      >
+        Cerca
+      </button>
+      <button
+        v-if="prodotti == null"
+        class="bg-blue-300 p-2 rounded block m-1 hover:bg-blue-400"
+        @click.prevent="getProdotti"
+      >
+        Carica prodotti
+      </button>
+    </div>
+    <div
+      class="flex lg:flex-row flex-col flex-wrap overflow-scroll lg:h-5/6 h-32 lg:relative lg:top-0 bottom-0 fixed w-full bg-blue-50"
+    >
+      <!-- bottoni -->
+      <button
+        v-for="prodotto in prodotti"
+        :key="prodotto"
+        class="box-content h-20 w-20 p-4 bg-white m-2 rounded-md shadow-md hover:bg-gray-100"
+        @click.prevent="
+          aggiungiArticolo({
+            reparto: prodotto.id_reparto,
+            quantita: 1,
+            descrizione: prodotto.desc_breve,
+            prezzo: prodotto.prezzo,
+          })
+        "
+      >
+        {{ prodotto.desc_breve }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import axios from "axios";
+import CryttoService from "../CryttoService";
+
+export default {
+  name: "articoli",
+  data() {
+    return {
+      filtro: "",
+      prodotti: null,
+    };
+  },
+  methods: {
+    aggiungiArticolo(data) {
+      if (
+        this.isNumeric(data.quantita) &
+        this.isNumeric(data.prezzo) &
+        this.isNumeric(data.reparto) &
+        this.isASCII(data.descrizione)
+      ) {
+        if (this.scontrino.length > 0) {
+          let aggiornato = false;
+          this.scontrino.forEach((el) => {
+            if (
+              (el.reparto == data.reparto) &
+              (el.descrizione == data.descrizione) &
+              (el.prezzo == data.prezzo)
+            ) {
+              data.quantita = el.quantita + data.quantita;
+              this.aggiornaArticolo(this.scontrino.indexOf(el), data);
+              aggiornato = true;
+            }
+          });
+          if (!aggiornato) {
+            this.$store.commit("aggiungiArticolo", data);
+          }
+        } else {
+          this.$store.commit("aggiungiArticolo", data);
+        }
+      } else {
+        window.alert("I dati inseriti non sono validi.");
+      }
+    },
+    aggiornaArticolo(index, data) {
+      this.$store.commit("cancellaArticolo", index);
+      this.$store.commit("aggiungiArticolo", data);
+    },
+    async getProdotti() {
+      var body = {};
+      body.token = this.token;
+      body.chiave = this.chiave;
+      let enc = await CryttoService.encrypt(JSON.stringify(body), this.pubkey);
+      var resEnc = await axios.post("db/get/prodotti", enc, {
+        headers: { "Content-Type": "text/plain" },
+      });
+      var res = JSON.parse(
+        await CryttoService.decSim(resEnc.data, this.chiave)
+      );
+      this.prodotti = res;
+    },
+    async getProdottiFiltrati() {
+      if (this.filtro == "") {
+        this.getProdotti();
+        return;
+      }
+      var body = {};
+      body.token = this.token;
+      body.chiave = this.chiave;
+      body.filtro = this.filtro;
+      let enc = await CryttoService.encrypt(JSON.stringify(body), this.pubkey);
+      var resEnc = await axios.post("db/get/prodotti_filtrati", enc, {
+        headers: { "Content-Type": "text/plain" },
+      });
+      var res = JSON.parse(
+        await CryttoService.decSim(resEnc.data, this.chiave)
+      );
+      this.prodotti = res;
+    },
+    isASCII(str) {
+      return /^[\x20-\x7F]*$/.test(str);
+    },
+    isNumeric(n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+    },
+  },
+  computed: {
+    ...mapGetters({ pubkey: "getPubkey" }),
+    ...mapGetters({ chiave: "getChiave" }),
+    ...mapGetters({ token: "getToken" }),
+    ...mapGetters({ scontrino: "getScontrino" }),
+  },
+};
+</script>
